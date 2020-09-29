2e26ba096219467c068521cdc72f40eb4926609d
hareshbhuriya <hareshybhuriya@gmail.com>
2020-09-29 21:49:41 +0530

    introduction to datatypes

---
+++ day1/datatypes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+
+    int i, j, k;
+    i = 20;
+    j = i % 2;
+    k = i / 2;
+
+    int authorName, publicationOwnerName, _kaushik, kaushi12, kaushi23kaushi, author_name;
+
+    long l = 312l;
+
+    float f1 = 10.12f;
+    double d = 23.345e+14;
+
+    char a = 'a';
+
+    printf("The i value is : %d\n", i);
+    printf("The j value is : %d\n", j);
+    printf("The k value is : %d\n", k);
+    //printf("The long values is : %ld\n", l);
+    //printf("The float values is : %f\n", f1);
+    //printf("The double value is : %lf\n", d);
+    //printf("The character value is : %c\n", a);
+
+    //printf("Hello world!\n");
+    //printf("Enter your age : ");
+    //scanf("%d", &i);
+    //printf("Your age is : %d\n", i);
+    return 0;
+
+}
